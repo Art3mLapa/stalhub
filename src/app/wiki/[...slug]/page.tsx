d82a74f5285f10c9c9be71b4d07318b34a7b98b3
oarer <oarer.yml@gmail.com>
2026-04-01 18:19:34 +0500

    fix metadata

--- src/app/wiki/[...slug]/page.tsx
+++ src/app/wiki/[...slug]/page.tsx
@@ -23,6 +23,8 @@ interface PageProps {
 	params: Promise<{ slug: string[] }>
 }
 
+export const dynamicParams = false
+
 export async function generateStaticParams() {
 	const slugs = await getAllWikiSlugs()
 	return slugs.map((slug) => ({
